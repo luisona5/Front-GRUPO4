f9ebe5bec18f7830ca1eda2b93312069b1f8b438
luisona5 <zoonajluis@gmail.com>
2025-08-12 04:15:39 -0500

    sesion estu

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -16,7 +16,7 @@ const Profile = () => {
                 <p className='mb-8'>Este módulo te permite gestionar el perfil del usuario</p>
             </div>
             {
-                user && user?.rol == "paciente"
+                user && user?.rol == "estudiante"
                     ? (<CardProfileOwner />)
                     : (
                         <div className='flex justify-around gap-x-8 flex-wrap gap-y-8 md:flex-nowrap'>
