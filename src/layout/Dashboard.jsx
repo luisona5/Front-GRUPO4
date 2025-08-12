e1c7d6a3514f7a5ee31dff8f68f8e2f02d825be5
luisona5 <zoonajluis@gmail.com>
2025-08-12 04:19:18 -0500

    sesion estu

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -21,7 +21,7 @@ const Dashboard = () => {
 
                 <img src="https://www.esan.edu.pe/images/blog/2015/07/04/futbol-peruano-figura-principal.jpg" alt="img-client" className="m-auto mt-8 p-1 border-2 border-slate-500 rounded-full" width={120} height={120} />
                 <p className='text-slate-400 text-center my-4 text-sm'> <span className='bg-green-600 w-3 h-3 inline-block rounded-full'>
-                </span> Bienvenido -{user?.nombre} </p>
+                </span> Bienvenido -{user?.nombre || user?.nombreEstudiante} </p>
                 <p className='text-slate-400 text-center my-4 text-sm'> Rol - {user?.rol}</p>
                 <hr className="mt-5 border-slate-500" />
 
@@ -49,7 +49,7 @@ const Dashboard = () => {
             <div className='flex-1 flex flex-col justify-between h-screen bg-gray-100'>
                 <div className='bg-gray-100 py-2 flex md:justify-end items-center gap-5 justify-center'>
                     <div className='text-md font-semibold text-slate-100'>
-                        Usuario - {user?.nombre}
+                        Usuario - {user?.nombre|| user?.nombreEstudiante}
                     </div>
                    
                     <div>
