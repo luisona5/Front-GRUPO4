5b5ba38f7fdb0474db77b7b82e70652fd8422f68
luisona5 <zoonajluis@gmail.com>
2025-08-20 21:36:45 -0500

    actualizando

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,12 +9,12 @@ export const Home = () => {
             >
 
                 <main className='text-center py-6 px-8 bg-blue-50 md:text-left md:flex justify-between items-center gap-10 md:py-1'>
-                <div className="flex justify-center gap-4">
-                    <Link to="/login" className='block bg-blue-700 w-40 py-2 mx-auto text-white rounded-2xl text-center sm:mx-0 hover:bg-yellow-700'>
-                        Get started
-                    </Link>
-                </div>
-            </main>
+                    <div className="flex justify-center gap-2">
+                        <Link to="/login" className='block bg-blue-700 w-40 py-2 mx-auto text-white rounded-2xl text-center sm:mx-0 hover:bg-yellow-700'>
+                            Get started
+                        </Link>
+                    </div>
+                </main>
 
             </div>
 
