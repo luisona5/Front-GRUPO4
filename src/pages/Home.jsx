c73c49648de564e4abf5fe298ccc5ce7f07ea4c3
luisona5 <zoonajluis@gmail.com>
2025-08-20 20:26:31 -0500

    actualizando

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,16 +5,20 @@ export const Home = () => {
         <>
             <div 
                 className="bg-fixed h-screen bg-cover bg-center"
-                style={{ backgroundImage: `url(./images/inicioDeporte.jpg)` }}
-            ></div>
+                style={{ backgroundImage: `url(./images/inicioDeporte.jpg)` }}   
+            >
 
-            <main className='text-center py-6 px-8 bg-blue-50 md:text-left md:flex justify-between items-center gap-10 md:py-1'>
+                <main className='text-center py-6 px-8 bg-blue-50 md:text-left md:flex justify-between items-center gap-10 md:py-1'>
                 <div className="flex justify-center gap-4">
                     <Link to="/login" className='block bg-blue-700 w-40 py-2 mx-auto text-white rounded-2xl text-center sm:mx-0 hover:bg-yellow-700'>
                         Get started
                     </Link>
                 </div>
             </main>
+
+            </div>
+
+            
         </>
     )
 }
