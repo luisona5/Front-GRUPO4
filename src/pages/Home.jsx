3f35c7daf7a660e9245624e8d688d574d436d2ae
luisona5 <zoonajluis@gmail.com>
2025-08-20 21:13:39 -0500

    actualizando

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ export const Home = () => {
     return (
         <>
             <div 
-                className="bg-fixed  bg-center   bg-contain h-screen w-screen"
+                className="bg-fixed  bg-center h-screen w-screen"
                 style={{ backgroundImage: `url(./images/deporte4.jpg)` }}   
             >
 
